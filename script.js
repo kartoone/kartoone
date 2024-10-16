725e7f5c15bd06139287cd95b5da13d3b576f046
Brian Toone <brtoone@samford.edu>
2024-10-16 12:50:30 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function downloadData() {
   event.preventDefault();
   var startdate = document.getElementById("startdate").value;
   var enddate = document.getElementById("enddate").value;
-  console.log("Starting date: " + start_date);
+  console.log("Starting date: " + startdate);
   console.log("Ending date: " + enddate);
+  fetch("https://compute.samford.edu/zohauth/clients/data");
 }
